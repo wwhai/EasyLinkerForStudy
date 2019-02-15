054b8956ca3d4cf7f48922bbcfb44a226710526a
wwhai <751957846@qq.com>
2019-02-15 19:21:39 +0800

    这个是V3 后期维护的版本

---
+++ src/main/java/com/easyiot/easylinker/EasylinkerApplication.java
@@ -0,0 +1,14 @@
+package com.easyiot.easylinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylinkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylinkerApplication.class, args);
+    }
+
+}
+
